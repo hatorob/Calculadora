fdb3e85d22907c2119a71a3e1c186822db3701ce
Alejandro Toro <alejotoro94@hotmail.com>
2020-07-10 22:10:30 -0500

    se decaro las variables de los botones en js

---
+++ script/main.js
@@ -0,0 +1,44 @@
+/* 
+MC -> borrar el contenido de la memoria
+MR -> visualizar el contenido de la memoria
+M- Guardar en la memoria con el signo -
+M+ guardar en la memoria con el signo +
+Mu
+Ac -> reinicializar la calculadora y su memoria
+c -> reinicializar la calculadora sin tocar su memoria
+
+*/
+
+
+
+const mc = document.getElementById('MC');
+const mr = document.getElementById('MR');
+const mmin = document.getElementById('m-');
+const mMas = document.getElementById("m+");
+const raiz = document.getElementById('raiz');
+const mu = document.getElementById('MU');
+const nn = document.getElementById('nn');
+const siete = document.getElementById('siete');
+const ocho = document.getElementById('ocho');
+const nueve = document.getElementById('nueve');
+const division = document.getElementById('division');
+const porcentaje = document.getElementById('porcentaje');
+const mm = document.getElementById('mm');
+const cuatro = document.getElementById('cuatro');
+const cinco = document.getElementById('cinco');
+const seis = document.getElementById('seis');
+const x = document.getElementById('x');
+const menos = document.getElementById('menos');
+const ac = document.getElementById('ac');
+const uno = document.getElementById('uno');
+const dos = document.getElementById('dos');
+const tres = document.getElementById('tres');
+const mas = document.getElementById('mas');
+const igual = document.getElementById('igual');
+const c = document.getElementById('c');
+const cero = document.getElementById('cero');
+const doubleCero = document.getElementById('doubleCero');
+const punto = document.getElementById('punto');
+
+
+
